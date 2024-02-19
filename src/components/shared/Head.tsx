69f3928372a20611a855b9332dadab6d69ee708b
juliscrep <julietascrepnik@gmail.com>
2024-02-19 16:30:51 -0300

    cambios en el codigo

--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -1,17 +1,17 @@
 import { Helmet } from 'react-helmet-async';
 
 type Props = {
-  title: string;
+  title?: string;
   description?: string;
 };
 
 const SERVICE_NAME = import.meta.env.VITE_SERVICE_NAME
 
-export const Head = ({ title, description }: Props) => (
+export const Head = ({ description }: Props) => (
   <Helmet>
-    <title>{`${title} | ${SERVICE_NAME}`}</title>
+    <title>{`${SERVICE_NAME}`}</title>
     <meta name="description" content={description ?? `This is ${SERVICE_NAME}`} />
-    <meta property="og:title" content={`${title} | ${SERVICE_NAME}`} />
+    <meta property="og:title" content={`${SERVICE_NAME}`} />
     <meta property="og:description" content={description ?? `This is ${SERVICE_NAME}`} />
     <meta name="robots" content="noindex" />
   </Helmet>
