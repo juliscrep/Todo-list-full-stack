93f6d0baabf8c764d99542bf24e0f8f54217fd2e
juliscrep <julietascrepnik@gmail.com>
2024-02-20 12:13:35 -0300

    cambios en el codigo

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react')) {
+            return 'react';
+          }
+          if (id.includes('firebase')) {
+            return 'firebase';
+          }
+          if(id.includes('tsconfigPaths')){
+            return 'tsconfigPaths';
+          }
+          
+        },
+      },
+    },
+  },
+});
